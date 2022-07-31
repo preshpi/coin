44171cf4715f37ebd86954aaf71ee0768af9c4fa
preshpi <egwuenuprecious7@gamil.com>
2022-07-31 18:08:28 +0100

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,31 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  darkMode: 'class',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'class', // false or 'media'
   theme: {
+    extend: { 
+    },
+
+    colors: {
+      'middle': '#191D21',
+      'light': '#EEEEEE',
+      'white': '#FFFFFF',
+      'red': '#EB1D36',
+      'body': '#131619',
+      'gray': '#4B5563',
+      'black': '#090910',
+      'rare': '#0B0D0F',
+      'blue': '#4954EF',
+      'grey': '#BCC1CA',
+      'orange': '#F77E21',
+      'green': '#3EC70B',
+      'yellow': '#FEB139',
+      'transparent':'transparent',
+    },
+    
+ 
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
-}
+};
