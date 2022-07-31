bab5cc5641cf50e1ef993ea9c4a2c3ed5d756167
preshpi <egwuenuprecious7@gamil.com>
2022-07-31 22:02:09 +0100

    first

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -56,11 +56,7 @@ function Navbar() {
 
 
                 <div className="flex mt-3 mx-2">
-                    <div className="grid lg:grid-cols-3  grid-cols-3 gap-4 cursor-pointer mt-4 lg:mt-2">
-                    <a href="#"><FiTwitter  className="dark:text-gray text-grey hover:text-gray dark:hover:text-white lg:text-xl transform transition-all hover:scale-125"/></a>
-                    <a href="#"><TbBrandTelegram  className="dark:text-gray text-grey hover:text-gray dark:hover:text-white lg:text-xl transform transition-all hover:scale-125"/></a>
-                    <a href="#"><FiInstagram  className="dark:text-gray text-grey hover:text-gray dark:hover:text-white lg:text-xl transform transition-all hover:scale-125"/></a>
-                    </div>
+             
                     <div className="mx-3">
                         <Toggle className=" dark:text-gray"/>
                     </div>
@@ -70,7 +66,7 @@ function Navbar() {
                             <button className="bg-blue rounded  px-1 py-1 text-white font-semibold hover:text-black ">Lightpaper</button>
                         </a>   
                         <a href="#">
-                            <button className="rounded mx-5 lg:mx-2 md:mx-3 px-1 py-1 dark:hover:text-white hover:text-black font-semibold border-2 text-gray">Telegram</button>
+                            <button className="rounded mx-2 lg:mx-2 md:mx-3 px-1 py-1 dark:hover:text-white hover:text-black font-semibold border-2 text-gray">Telegram</button>
                         </a>   
 
                     </div>
@@ -604,13 +600,73 @@ function Navbar() {
                </div>
             </div>    
 
-            <div  className="container-lg text-center bg-blue m-6 rounded-lg h-64 md:h-40">
-                <div className="p-8 lg:p-20 md:p-12">
+            <div  className="container-lg text-center bg-blue m-6 rounded-lg lg:h-64 h-64">
+                <div className="p-8 lg:p-18 md:p-12">
                 <h3 className="text-white text-center text-xl">You're early, want to find out how to get access in the sale?</h3>
-               <div className=" text-center">
+                <div className=" text-center">
                     <a href="#"><button className="bg-white  text-blue rounded-lg py-2 px-5 mt-4 mb-6 mx-2">Ask in telegram</button></a> 
                     <a href="#"><button className="bg-white  text-blue rounded-lg py-2 px-5 lg:mt-4 mb-3 mx-2">Direct email</button></a>
                 </div> 
+             
+                <div className="flex text-center justify-center">
+                <a href="#!" type="button" class="rounded-full border-2 border-white text-white leading-normal uppercase hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1">
+                    <svg aria-hidden="true"
+                    focusable="false"
+                    data-prefix="fab"
+                    data-icon="facebook-f"
+                    class="w-2 h-full mx-auto"
+                    role="img"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 320 512"
+                    >
+                    <path
+                    fill="currentColor"
+                    d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"
+                    ></path>
+                    </svg>
+                </a>
+
+                <a href="#!" type="button" class="rounded-full border-2 border-white text-white leading-normal uppercase hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1">
+                    <svg aria-hidden="true"
+                    focusable="false"
+                    data-prefix="fab"
+                    data-icon="twitter"
+                    class="w-3 h-full mx-auto"
+                    role="img"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                    >
+                    <path
+                        fill="currentColor"
+                        d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"
+                    ></path>
+                    </svg>
+                </a>
+
+                <a href="#!" type="button" class="rounded-full border-2 border-white text-white leading-normal uppercase hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1">
+                    <svg aria-hidden="true"
+                    focusable="false"
+                    data-prefix="fab"
+                    data-icon="linkedin-in"
+                    class="w-3 h-full mx-auto"
+                    role="img"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 448 512"
+                    >
+                    <path
+                        fill="currentColor"
+                        d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"
+                    ></path>
+                    </svg>
+                </a>
+
+
+                </div>
+
+                <div className="text-center text-gray-700 p-3">
+                © 2022 Copyright
+                <a className="text-gray-800"></a>
+                </div>
                 </div>
             
 
